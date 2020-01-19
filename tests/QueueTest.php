9770f8e1c67cc07d05eafafa6030489ec51dcc77
Carlos <amorescarlos93@hotmail.com>
2020-01-19 11:19:41 +0100

    Section 4: Unit test a Queue class, @depends

--- tests/QueueTest.php
+++ tests/QueueTest.php
@@ -24,4 +24,30 @@ class QueueTest extends TestCase
         $queue->pop();
         $this->assertEquals(0, $queue->getCount());
     }
+
+    public function testNewQueueIsEmptyIndep()
+    {
+        $queue = new Queue();
+        $this->assertEquals(0, $queue->getCount());
+        return $queue;
+    }
+
+    /**
+     * @depends testNewQueueIsEmptyIndep
+     */
+    public function testAnItemIsAddedToTheQueueIndep(Queue $queue)
+    {
+        $queue->push('klk');
+        $this->assertEquals(1, $queue->getCount());
+    }
+
+    /**
+     * @depends testNewQueueIsEmptyIndep
+     */
+    public function testAnItemIsRemoveToTheQueueIndep(Queue $queue)
+    {
+        $queue->push('klk');
+        $queue->pop();
+        $this->assertEquals(1, $queue->getCount());
+    }
 }
